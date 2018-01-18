946f2af4087a614cc5d954888323181e231b14ff
leon <444925473@qq.com>
2018-01-18 10:44:42 +0800

    第一次提交 第二次提交

--- app/src/main/java/com/example/leon/xiaoshixun_day01_git/MainActivity.java
+++ app/src/main/java/com/example/leon/xiaoshixun_day01_git/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.leon.xiaoshixun_day01_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        //修改过后的代码
+        Toast.makeText( this, "男神最爱小燕燕", Toast.LENGTH_SHORT ).show();
     }
 }
